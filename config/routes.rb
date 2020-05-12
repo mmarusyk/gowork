a9fc60f5b7341e3a6b79d8efacd0d5840a0bb4a1
Mick <misha.marusyk@gmail.com>
2020-05-12 14:16:35 +0300

    Generate controller User new

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   get '/home', to: 'static_pages#home'
+  get '/signup', to: 'users#new'
 end
