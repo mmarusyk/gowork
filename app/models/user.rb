728f8840ba56b1529b2503fd81d2ee242f14fdd1
Mick <misha.marusyk@gmail.com>
2020-05-12 17:01:38 +0300

    Save email in downcase

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
   validates :first_name, presence: true, length: { minimum: 3, maximum: 50 }
   validates :last_name, presence: true, length: { minimum: 3, maximum: 50 }
   validates :city, presence: true, length: { minimum: 3, maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true,
                     length: { maximum: 255 },
-                    format: { with: VALID_EMAIL_REGEX },uniqueness: {case_sensitive: false}
+                    format: { with: VALID_EMAIL_REGEX }, uniqueness: true
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -59,7 +59,6 @@ class UserTest < ActiveSupport::TestCase
   end
   test 'email addresses should be unique' do
     duplicate_user = @user.dup
-    duplicate_user.email = @user.email.upcase 
     @user.save
     assert_not duplicate_user.valid?
   end
