7b4dfcdd918fdf903e8843d63cc71a4ff50b487c
Mick <misha.marusyk@gmail.com>
2020-05-13 13:59:12 +0300

    Add authorization

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: %i[edit update]
+  before_action :correct_user, only: %i[edit update]
+
   def show
     @user = User.find(params[:id])
   end
@@ -30,6 +33,19 @@ class UsersController < ApplicationController
     else
       render 'edit'
     end
+  end
+
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
   end
 
   def user_params
