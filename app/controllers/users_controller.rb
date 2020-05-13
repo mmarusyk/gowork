f4dc71825caa077f68880b152a928562eed1f34a
Mick <misha.marusyk@gmail.com>
2020-05-13 13:36:59 +0300

    Add description in user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,6 +33,14 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :city, :password, :password_confirmation)
+    params.require(:user).permit(
+      :first_name,
+      :last_name,
+      :email,
+      :city,
+      :description,
+      :password,
+      :password_confirmation
+    )
   end
 end
