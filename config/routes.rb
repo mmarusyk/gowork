93542bc366c85b89d2aceb10fdc5a65f29ae4372
Mick <misha.marusyk@gmail.com>
2020-05-13 08:40:49 +0300

    Create session controller new and form

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   get '/about', to: 'static_pages#about'
   get '/home', to: 'static_pages#home'
   get '/signup', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'session#destroy'
   resources :users
 end
