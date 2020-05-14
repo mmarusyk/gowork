70100555c3198d0d5063d5bfd65c5545a73c5306
Mick <misha.marusyk@gmail.com>
2020-05-14 11:31:53 +0300

    Add reset pass

--- app/models/user.rb
+++ app/models/user.rb
@@ -56,7 +56,11 @@ class User < ApplicationRecord
 
   def send_password_reset_email
     UserMailer.password_reset(self).deliver_now
-  end 
+  end
+
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
 
   private
   def downcase_email
