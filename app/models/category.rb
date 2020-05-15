260503a4a760129610110fdb8ce79c10c437396c
Mick <misha.marusyk@gmail.com>
2020-05-15 15:22:52 +0300

    Fix references

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,4 @@
 class Category < ApplicationRecord
-  has_one :orders, dependent: :destroy
+  has_many :orders, dependent: :destroy
   validates :title, presence: true
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,7 @@
 class Order < ApplicationRecord
   belongs_to :user
-  default_scope -> { order(created_at: :desc) }
   belongs_to :category
+  default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   validates :category_id, presence: true
   validates :title, presence: true, length: { maximum: 50 }
