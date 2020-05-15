af1349cdb6832906b6061508d3d384d20092b175
Mick <misha.marusyk@gmail.com>
2020-05-15 09:42:17 +0300

    Add price to orders and create tests

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,10 @@
 class Order < ApplicationRecord
   belongs_to :user
   belongs_to :category
+  validates :user_id, presence: true
+  validates :category_id, presence: true
+  validates :title, presence: true, length: { maximum: 50 }
+  validates :description, presence: true, length: { maximum: 500 }
+  validates :city, presence: true
+  validates :price, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_15_060355) do
+ActiveRecord::Schema.define(version: 2020_05_15_062936) do
 
   create_table "categories", force: :cascade do |t|
     t.string "title"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2020_05_15_060355) do
     t.integer "category_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.decimal "price", precision: 8, scale: 2
     t.index ["category_id", "created_at"], name: "index_orders_on_category_id_and_created_at"
     t.index ["category_id"], name: "index_orders_on_category_id"
     t.index ["user_id", "created_at"], name: "index_orders_on_user_id_and_created_at"
