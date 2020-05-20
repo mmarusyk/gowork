d020e2158d099729a4362389c59c3fd40b7a9c57
Mick <misha.marusyk@gmail.com>
2020-05-20 10:48:53 +0300

    Some fix

--- app/models/order.rb
+++ app/models/order.rb
@@ -14,8 +14,11 @@ class Order < ApplicationRecord
 
   def self.search_by(title_or_description, category_id, city, min_price, max_price)
     min_price = -1 if min_price.empty?
-    max_price = 1000000000 if max_price.empty?
-    where("category_id = '#{category_id}' AND
-      ' ' || (LOWER(title) || ' ' LIKE '%#{title_or_description.downcase}%' OR '#{title_or_description}' IS NULL OR ' ' || (LOWER(description) || ' ' LIKE '%#{title_or_description.downcase}%' OR '#{title_or_description}' IS NULL AND ' ' || (LOWER(city) || ' ' LIKE '%#{city.downcase}%' OR '#{city}' IS NULL AND price >= #{min_price} AND price <= #{max_price} ")
+    max_price = 1_000_000_000 if max_price.empty?
+    where("category_id = '#{category_id}' AND 
+      ((' ' || (LOWER(title) || ' ' LIKE '%#{title_or_description.downcase}%' OR '#{title_or_description}' IS NULL) OR 
+      (' ' || (LOWER(description) || ' ' LIKE '%#{title_or_description.downcase}%' OR '#{title_or_description}' IS NULL)) AND 
+      (' ' || (LOWER(city) || ' ' LIKE '%#{city.downcase}%' OR '#{city}' IS NULL) AND 
+      price >= #{min_price} AND price <= #{max_price} ")
   end
 end
